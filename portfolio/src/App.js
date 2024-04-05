2d38819fb38dc82de3d720eaac6b8528419f9304
Bozeyy <spereaux2@gmail.com>
2024-04-05 14:11:38 +0200

    starting hb page

---
+++ portfolio/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Header } from './components/Header';
+import "./css/accueil.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div className='content'>
+        a
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../css/header.css';
+
+export const Header = () => {
+
+    return (
+        <div className='header'>
+            <div className='left_header'>
+                <div className='logo'>
+                    <div className='circle'>
+                    </div>
+                    <h1>S</h1>
+                    <h2>imon Pereaux</h2>
+                </div>
+            </div>
+            <div className='right_header'>
+                
+            </div>
+        </div>  
+    );
+}
