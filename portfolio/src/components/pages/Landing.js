740a5293b9cd7f995a33fe4422de6ce25962b244
Bozeyy <spereaux2@gmail.com>
2024-05-24 15:39:41 +0200

    création du squelette du bureau

---
+++ portfolio/src/components/pages/Landing.js
@@ -0,0 +1,20 @@
+// src/components/pages/Landing.js
+import React from 'react';
+import "./../../styles/Landing.css";
+
+const Landing = () => {
+    return (
+        <div className="landing-container">
+            <div className='content-desktop'>
+
+            </div>
+            <div className="task-bar">
+                <div className="start-icon">
+                    
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Landing;
